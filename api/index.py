e4b7f16f5a67d239c0ff8e7261a61d823b29cc8d
esasardar8-hash <esasardar8@gmail.com>
2026-01-29 16:51:54 +0300

    Import GenerationConfig from google.generativeai.types

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
+from google.generativeai.types import GenerationConfig
 import os
 
 app = Flask(__name__)
